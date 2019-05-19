0da4d71020482e6697617840d52cf04ce30ce65a
conor42 <ronoc74@yahoo.com>
2019-05-20 00:31:03 +1000

    Use threading and header macros to select atomic funcs

--- src/liblzma/fast-lzma2/atomic.h
+++ src/liblzma/fast-lzma2/atomic.h
@@ -17,23 +17,7 @@ extern "C" {
 
 /* atomic add */
 
-#if !defined(FL2_SINGLETHREAD) && defined(_WIN32)
-
-#ifdef WINVER
-#undef WINVER
-#endif
-#define WINVER       0x0600
-
-#ifdef _WIN32_WINNT
-#undef _WIN32_WINNT
-#endif
-#define _WIN32_WINNT 0x0600
-
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
-
-#include <windows.h>
+#if defined(MYTHREAD_ENABLED) && defined(_WIN32)
 
 
 typedef LONG volatile FL2_atomic;
@@ -42,7 +26,9 @@ typedef LONG volatile FL2_atomic;
 #define FL2_atomic_add(n, a) InterlockedAdd(&n, a)
 #define FL2_nonAtomic_increment(n) (++n)
 
-#elif !defined(FL2_SINGLETHREAD) && defined(__GNUC__)
+
+#elif defined(MYTHREAD_ENABLED) && defined(__GNUC__)
+
 
 typedef long FL2_atomic;
 #define ATOMIC_INITIAL_VALUE 0
@@ -50,7 +36,9 @@ typedef long FL2_atomic;
 #define FL2_atomic_add(n, a) __sync_fetch_and_add(&n, a)
 #define FL2_nonAtomic_increment(n) (n++)
 
-#elif !defined(FL2_SINGLETHREAD) && defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L) && !defined(__STDC_NO_ATOMICS__) /* C11 */
+
+#elif defined(MYTHREAD_ENABLED) && defined(HAVE_STDATOMIC_H) /* C11 */
+
 
 #include <stdatomic.h>
 
@@ -60,10 +48,12 @@ typedef _Atomic long FL2_atomic;
 #define FL2_atomic_add(n, a) atomic_fetch_add(&n, a)
 #define FL2_nonAtomic_increment(n) (n++)
 
+
 #else  /* No atomics */
 
-#	ifndef FL2_SINGLETHREAD
-#		error  No atomic operations available. Change compiler config or define FL2_SINGLETHREAD for the entire build.
+
+#	ifdef MYTHREAD_ENABLED
+#		error  No atomic operations available. Use --disable-threads to configure a single-threaded build.
 #	endif
 
 typedef long FL2_atomic;
@@ -72,7 +62,8 @@ typedef long FL2_atomic;
 #define FL2_atomic_add(n, a) (n += (a))
 #define FL2_nonAtomic_increment(n) (n++)
 
-#endif /* FL2_SINGLETHREAD */
+
+#endif /* atomics */
 
 
 #if defined (__cplusplus)
